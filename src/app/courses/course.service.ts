3456e0a478e2e48464b2be4bf7f3fd6436fc1dbf
Clemente <clementesc@gmail.com>
2021-08-27 12:46:54 -0300

    Criado método delete.

--- src/app/courses/course.service.ts
+++ src/app/courses/course.service.ts
@@ -30,4 +30,8 @@ export class CourseService {
             return this.httpClient.post<Course>(`${this.coursesUrl}`, course);
         }
     }
+
+    deleteById(id: number): Observable<any> {
+        return this.httpClient.delete<any>(`${this.coursesUrl}/${id}`);
+    }
 }
