071058e220471bbb5d4efe0156ce5d35ffaecd45
Clemente <clementesc@gmail.com>
2021-08-27 11:09:54 -0300

    Criação do formulário de edição de cursos

--- src/app/courses/course.service.ts
+++ src/app/courses/course.service.ts
@@ -11,6 +11,18 @@ export class CourseService {
         return COURSES;
     }
 
+    retrieveById(id: number): Course { 
+        return COURSES.find((courseInterator: Course) => courseInterator.id === id);
+    }
+
+    save(course: Course): void {
+        // Update
+        if (course.id){
+            const index = COURSES.findIndex((courseInterator: Course) => courseInterator.id === course.id);
+            COURSES[index] = course;
+        }
+    }
+
 }
 
 var COURSES: Course[] = [
